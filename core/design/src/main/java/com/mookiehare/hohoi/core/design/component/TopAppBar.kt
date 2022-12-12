9df85fe41129aa0ba77090f00fcafbe8bafcc830
hare <khm0651@gmail.com>
2022-12-12 14:13:51 +0900

    feat: add top app bar

---
+++ core/design/src/main/java/com/mookiehare/hohoi/core/design/component/TopAppBar.kt
@@ -0,0 +1,62 @@
+package com.mookiehare.hohoi.core.design.component
+
+import androidx.annotation.StringRes
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.mookiehare.hohoi.core.design.icon.HoHoiIcons
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HoHoiTopAppBar(
+    @StringRes titleRes: Int,
+    modifier: Modifier = Modifier,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
+    actions: List<TopAppBarAction>
+){
+    TopAppBar(
+        title = { Text(text = stringResource(id = titleRes)) },
+        actions = {
+            actions.forEach{ topAppBarAction ->
+                IconButton(onClick = topAppBarAction.onActionClick) {
+                    Icon(
+                        imageVector = topAppBarAction.actionIcon,
+                        contentDescription = topAppBarAction.actionIconContentDescription,
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            }
+        },
+        colors = colors,
+        modifier = modifier
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview("Top App Bar")
+@Composable
+fun HoHoiTopAppBarPreview(){
+    HoHoiTopAppBar(
+        titleRes = android.R.string.unknownName,
+        actions = listOf(
+            TopAppBarAction(
+                actionIcon = HoHoiIcons.MoreVert,
+            ),
+            TopAppBarAction(
+                actionIcon = HoHoiIcons.MoreVert,
+            ),
+            TopAppBarAction(
+                actionIcon = HoHoiIcons.MoreVert,
+            )
+        )
+    )
+}
+
+data class TopAppBarAction(
+    val actionIcon: ImageVector,
+    val actionIconContentDescription: String = "",
+    val onActionClick: () -> Unit = {}
+)
---
+++ core/design/src/main/java/com/mookiehare/hohoi/core/design/icon/HoHoiIcons.kt
@@ -0,0 +1,9 @@
+package com.mookiehare.hohoi.core.design.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+
+object HoHoiIcons {
+    val MoreVert = Icons.Default.MoreVert
+
+}
