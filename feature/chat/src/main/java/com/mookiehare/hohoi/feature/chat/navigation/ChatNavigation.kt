827adbccf21704bf4154410d25e578725682d7a1
hare <khm0651@gmail.com>
2023-01-16 00:26:31 +0900

    feat: add chat detail navigation

--- feature/chat/src/main/java/com/mookiehare/hohoi/feature/chat/navigation/ChatNavigation.kt
+++ feature/chat/src/main/java/com/mookiehare/hohoi/feature/chat/navigation/ChatNavigation.kt
@@ -1,19 +1,51 @@
 package com.mookiehare.hohoi.feature.chat.navigation
 
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.*
 import androidx.navigation.compose.composable
+import com.mookiehare.hohoi.feature.chat.chatDetail.ChatDetailRoute
 import com.mookiehare.hohoi.feature.chat.chatList.ChatListRoute
 
+const val chatGraph = "chat_graph"
 const val chatNavigationRoute = "chat_route"
+
+const val chatDetailNavigationRoute = "chat_detail_route"
+const val chatDetailIdArg = "chatDetailId"
+
+internal class ChatDetailArgs(val chatDetailId: String) {
+    constructor(savedStateHandle: SavedStateHandle) :
+            this(checkNotNull(savedStateHandle.get(chatDetailIdArg) as? String))
+}
 
 fun NavController.navigateToChat(navOptions: NavOptions? = null) {
     this.navigate(chatNavigationRoute, navOptions)
 }
 
-fun NavGraphBuilder.chatScreen() {
-    composable(route = chatNavigationRoute) {
-        ChatListRoute()
+fun NavController.navigateToChatDetail(chatDetailId: String){
+    this.navigate("$chatDetailNavigationRoute/$chatDetailId")
+}
+
+fun NavGraphBuilder.chatScreen(
+    navigateToChatDetail: (String) -> Unit,
+    nestedGraphs: NavGraphBuilder.() -> Unit
+) {
+    navigation(
+        route = chatGraph,
+        startDestination = chatNavigationRoute
+    ) {
+        composable(route = chatNavigationRoute) {
+            ChatListRoute(navigateToChatDetail = navigateToChatDetail)
+        }
+
+        composable(
+            route = "$chatDetailNavigationRoute/{$chatDetailIdArg}",
+            arguments = listOf(
+                navArgument(chatDetailIdArg) { type = NavType.StringType }
+            )
+        ) {
+            ChatDetailRoute()
+        }
+
+        nestedGraphs()
     }
 }
