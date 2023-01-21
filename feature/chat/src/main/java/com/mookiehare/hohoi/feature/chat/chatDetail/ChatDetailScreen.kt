301ab2cfc2cc5e7f9f2561621928413aadd18b1b
hare <khm0651@gmail.com>
2023-01-21 17:36:11 +0900

    feat: add Reply Component

--- feature/chat/src/main/java/com/mookiehare/hohoi/feature/chat/chatDetail/ChatDetailScreen.kt
+++ feature/chat/src/main/java/com/mookiehare/hohoi/feature/chat/chatDetail/ChatDetailScreen.kt
@@ -9,37 +9,53 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mookiehare.hohoi.core.design.component.ImageProfile
-import com.mookiehare.hohoi.core.design.extensions.drawNinePatch
 import com.mookiehare.hohoi.core.design.theme.HohoiTheme
-import com.mookiehare.hohoi.feature.chat.R
 import com.mookiehare.hohoi.feature.chat.model.*
 
-val myTextMessageDummyData = MyText(
+val myTextMessageDummyData = Text(
     id = "",
     content = "꽃이 예쁘게 피었네요.) ㅁㄴㅇㄹㅁㄴㅇㄹasdflkajsdlkfjasldkjflaksjdfasdflaksjdflkjasldkfjlaksjdfㅁㄴㅇ리ㅏㅁ너이ㅏ러ㅣㅏㅁ너이ㅏ러 ㅁ니ㅏ어리ㅏㅁ넝리ㅏㅓㅁ니ㅏ어리마ㅓㄴㅁ니ㅏ어리ㅏㅁ너이ㅏ러 만어ㅣ라ㅓㅁ니ㅏㅁㄴㅇ람,눙,룸,느울,ㅜㅁ미넝리머니아러ㅣㅏㅁ너ㅣ라ㅓㅣ마너 이라ㅓㅁ니ㅏㅇ ㅓ린마ㅓㅇ ㅣ럼니 ㅏㅓ리ㅏㅁ너 ㅣ라ㅓㅁ니아 ㅓ림나ㅓ ㅣ라머니 아러ㅣㅁ나ㅓㅇ ㅣ라머닝 ㅏㅓ림ㄴ ㅏ어리ㅏ너 ㅣㅇ라ㅓ미 나어리 ㅏㅓㅁ니아 ㅓㅣㅁ나ㅓㅇㄹ ㅣㅏ머니 아러미나ㅓㅇ 림너 아ㅣㄹ머니 ㅏㅇ러ㅣㅁ ㅏ넝ㄹ",
-    isDelete = false,
-    isReport = false,
-    sendTime = "오후 1:24"
+    sendTime = "오후 1:24",
+    from = From.My
 )
 
-val otherTextMessageDummyData = OtherText(
+val otherTextMessageDummyData = Text(
     id = "",
-    nickname = "곽하민",
-    profileUrl = "https://static01.nyt.com/images/2014/08/24/arts/24GRANDE1/24JPGRANDE1-superJumbo.jpg",
     content = "꽃이 예쁘게 피었네요.) ㅁㄴㅇㄹㅁㄴㅇㄹasdflkajsdlkfjasldkjflaksjdfasdflaksjdflkjasldkfjlaksjdfㅁㄴㅇ리ㅏㅁ너이ㅏ러ㅣㅏㅁ너이ㅏ러 ㅁ니ㅏ어리ㅏㅁ넝리ㅏㅓㅁ니ㅏ어리마ㅓㄴㅁ니ㅏ어리ㅏㅁ너이ㅏ러 만어ㅣ라ㅓㅁ니ㅏㅁㄴㅇ람,눙,룸,느울,ㅜㅁ미넝리머니아러ㅣㅏㅁ너ㅣ라ㅓㅣ마너 이라ㅓㅁ니ㅏㅇ ㅓ린마ㅓㅇ ㅣ럼니 ㅏㅓ리ㅏㅁ너 ㅣ라ㅓㅁ니아 ㅓ림나ㅓ ㅣ라머니 아러ㅣㅁ나ㅓㅇ ㅣ라머닝 ㅏㅓ림ㄴ ㅏ어리ㅏ너 ㅣㅇ라ㅓ미 나어리 ㅏㅓㅁ니아 ㅓㅣㅁ나ㅓㅇㄹ ㅣㅏ머니 아러미나ㅓㅇ 림너 아ㅣㄹ머니 ㅏㅇ러ㅣㅁ ㅏ넝ㄹ",
-    isDelete = false,
-    isReport = false,
-    sendTime = "오후 1:24"
+    sendTime = "오후 1:24",
+    from = From.Other(
+        nickname = "곽하민",
+        profileUrl = "https://static01.nyt.com/images/2014/08/24/arts/24GRANDE1/24JPGRANDE1-superJumbo.jpg",
+    )
+)
+
+val myReplyForTextMessageDummyData = ReplyText(
+    id = "",
+    replyId = "",
+    replyNickname = "곽하민",
+    replyContent = "꽃이 예쁘게 피었네요.) ㅁㄴㅇㄹㅁㄴㅇㄹasdflkajsdlkfjasldkjflaksjdfasdflaksjdflkjasldkfjlaksjdfㅁㄴㅇ리ㅏㅁ너이ㅏ러ㅣㅏㅁ너이ㅏ러 ㅁ니ㅏ어리ㅏㅁ넝리ㅏㅓㅁ니ㅏ어리마ㅓㄴㅁ니ㅏ어리ㅏㅁ너이ㅏ러 만어ㅣ라ㅓㅁ니ㅏㅁㄴㅇ람,눙,룸,느울,ㅜㅁ미넝리머니아러ㅣㅏㅁ너ㅣ라ㅓㅣ마너 이라ㅓㅁ니ㅏㅇ ㅓ린마ㅓㅇ ㅣ럼니 ㅏㅓ리ㅏㅁ너 ㅣ라ㅓㅁ니아 ㅓ림나ㅓ ㅣ라머니 아러ㅣㅁ나ㅓㅇ ㅣ라머닝 ㅏㅓ림ㄴ ㅏ어리ㅏ너 ㅣㅇ라ㅓ미 나어리 ㅏㅓㅁ니아 ㅓㅣㅁ나ㅓㅇㄹ ㅣㅏ머니 아러미나ㅓㅇ 림너 아ㅣㄹ머니 ㅏㅇ러ㅣㅁ ㅏ넝ㄹ",
+    content = "ㅎㅇㅎㅇ",
+    sendTime = "오후 1:24",
+    from = From.My
+)
+
+val otherReplyForTextMessageDummyData = ReplyText(
+    id = "",
+    replyId = "",
+    replyNickname = "강철",
+    replyContent = "꽃이 예쁘게 피었네요.) ㅁㄴㅇㄹㅁㄴㅇㄹasdflkajsdlkfjasldkjflaksjdfasdflaksjdflkjasldkfjlaksjdfㅁㄴㅇ리ㅏㅁ너이ㅏ러ㅣㅏㅁ너이ㅏ러 ㅁ니ㅏ어리ㅏㅁ넝리ㅏㅓㅁ니ㅏ어리마ㅓㄴㅁ니ㅏ어리ㅏㅁ너이ㅏ러 만어ㅣ라ㅓㅁ니ㅏㅁㄴㅇ람,눙,룸,느울,ㅜㅁ미넝리머니아러ㅣㅏㅁ너ㅣ라ㅓㅣ마너 이라ㅓㅁ니ㅏㅇ ㅓ린마ㅓㅇ ㅣ럼니 ㅏㅓ리ㅏㅁ너 ㅣ라ㅓㅁ니아 ㅓ림나ㅓ ㅣ라머니 아러ㅣㅁ나ㅓㅇ ㅣ라머닝 ㅏㅓ림ㄴ ㅏ어리ㅏ너 ㅣㅇ라ㅓ미 나어리 ㅏㅓㅁ니아 ㅓㅣㅁ나ㅓㅇㄹ ㅣㅏ머니 아러미나ㅓㅇ 림너 아ㅣㄹ머니 ㅏㅇ러ㅣㅁ ㅏ넝ㄹ",
+    content = "크크",
+    sendTime = "오후 1:24",
+    from = From.Other(
+        nickname = "곽하민",
+        profileUrl = "https://static01.nyt.com/images/2014/08/24/arts/24GRANDE1/24JPGRANDE1-superJumbo.jpg",
+    )
 )
 
 @Composable
@@ -71,6 +87,12 @@ internal fun ChatDetailScreen(
         item {
             Message(otherTextMessageDummyData)
         }
+        item {
+            Message(otherReplyForTextMessageDummyData)
+        }
+        item {
+            Message(myReplyForTextMessageDummyData)
+        }
     }
 }
 
@@ -79,43 +101,33 @@ fun Message(
     message: Message
 ){
     Row(
-        horizontalArrangement = if(message is FromMy) Arrangement.End else Arrangement.Start,
+        horizontalArrangement = if(message.from is From.My) Arrangement.End else Arrangement.Start,
         modifier = Modifier.fillMaxWidth()
     ) {
-        if(message is FromOther) {
-            ImageProfile(profileUrl = message.profileUrl)
+        if(message.from is From.Other) {
+            ImageProfile(profileUrl = (message.from as From.Other).profileUrl)
             Spacer(modifier = Modifier.padding(ChatComponentStyle.smallSpacing))
         }
 
-        AuthorAndTextMessage(message = message)
+        AuthorAndMessage(message = message)
     }
 }
 
 @Composable
-fun AuthorAndTextMessage(
+fun AuthorAndMessage(
     message: Message
 ){
     Column {
-        when(message) {
-            is FromOther -> {
-                Text(
-                    text = message.nickname,
-                    fontSize = ChatComponentStyle.normalFontSize
-                )
+        if(message.from is From.Other) {
+            Text(
+                text = (message.from as From.Other).nickname,
+                style = MaterialTheme.typography.bodyMedium.copy(color = LocalContentColor.current),
+            )
 
-                Spacer(modifier = Modifier.padding(ChatComponentStyle.smallSpacing))
-
-                when (message) {
-                    is OtherText -> OtherTextMessageBubble(message = message)
-                }
-            }
-
-            is FromMy -> {
-                when (message) {
-                    is MyText -> MyTextMessageBubble(message = message)
-                }
-            }
+            Spacer(modifier = Modifier.padding(ChatComponentStyle.smallSpacing))
         }
+
+        MessageAndSendTime(message = message)
     }
 }
 
@@ -123,64 +135,95 @@ private val OtherChatBubbleShape = RoundedCornerShape(4.dp, 20.dp, 20.dp, 20.dp)
 private val MyChatBubbleShape = RoundedCornerShape(20.dp, 4.dp, 20.dp, 20.dp)
 
 @Composable
-fun MyTextMessageBubble(
-    message: MyText,
+fun MessageAndSendTime(
+    message: Message,
 ){
-    Row(verticalAlignment = Alignment.Bottom) {
-        Spacer(modifier = Modifier.padding(ChatComponentStyle.smallSpacing))
+    val backgroundColor = if(message.from == From.My) {
+        MaterialTheme.colorScheme.primary
+    } else {
+        MaterialTheme.colorScheme.tertiary
+    }
 
-        Text(
-            text = message.sendTime,
-            fontSize = ChatComponentStyle.smallFontSize,
-        )
+    val shape = if(message.from == From.My) {
+        MyChatBubbleShape
+    } else {
+        OtherChatBubbleShape
+    }
+
+    Row(verticalAlignment = Alignment.Bottom) {
+        if(message.from == From.My){
+            Spacer(modifier = Modifier.padding(ChatComponentStyle.smallSpacing))
+
+            Text(
+                text = message.sendTime,
+                style = MaterialTheme.typography.bodySmall.copy(color = LocalContentColor.current),
+            )
+        }
 
         Surface(
-            color = MaterialTheme.colorScheme.tertiary,
-            shape = MyChatBubbleShape
+            modifier = Modifier.widthIn(max = ChatComponentStyle.maxWidth),
+            color = backgroundColor,
+            shape = shape
         ) {
+            when(message){
+                is Text -> TextMessageBubble(message = message)
+                is ReplyText -> ReplyForTextMessageBubble(message = message)
+            }
+        }
+
+        if(message.from is From.Other){
+            Spacer(modifier = Modifier.padding(ChatComponentStyle.smallSpacing))
+
             Text(
-                text = message.content,
-                modifier = Modifier
-                    .widthIn(max = ChatComponentStyle.maxWidth)
-                    .padding(16.dp, 8.dp, 8.dp, 8.dp),
-                style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
+                text = message.sendTime,
+                style = MaterialTheme.typography.bodySmall.copy(color = LocalContentColor.current),
             )
         }
     }
 }
 
 @Composable
-fun OtherTextMessageBubble(
-    message: OtherText,
-){
-    Row(verticalAlignment = Alignment.Bottom) {
-        Surface(
-            color = MaterialTheme.colorScheme.primary,
-            shape = OtherChatBubbleShape
+fun TextMessageBubble(message: Text) {
+    Text(
+        modifier = Modifier.padding(10.dp),
+        text = message.content,
+        style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
+    )
+}
+
+@Composable
+fun ReplyForTextMessageBubble(message: ReplyText){
+    Column {
+        Column(
+            modifier = Modifier.padding(10.dp),
         ) {
             Text(
-                text = message.content,
-                modifier = Modifier
-                    .widthIn(max = ChatComponentStyle.maxWidth)
-                    .padding(16.dp, 8.dp, 8.dp, 8.dp),
-                style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
+                text = "${message.replyNickname}에게 답장",
+                style = MaterialTheme.typography.titleMedium.copy(color = LocalContentColor.current),
+            )
+            Spacer(modifier = Modifier.padding(1.dp))
+            Text(
+                text = message.replyContent,
+                style = MaterialTheme.typography.bodyMedium.copy(color = LocalContentColor.current),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
             )
         }
 
-        Spacer(modifier = Modifier.padding(ChatComponentStyle.smallSpacing))
+        Divider(
+            thickness = 0.5.dp,
+            color = Color.Gray
+        )
 
         Text(
-            text = message.sendTime,
-            fontSize = ChatComponentStyle.smallFontSize,
+            modifier = Modifier.padding(10.dp),
+            text = message.content,
+            style = MaterialTheme.typography.bodyLarge.copy(color = LocalContentColor.current),
         )
     }
 }
 
 private object ChatComponentStyle {
-    val smallFontSize = 10.sp
-    val normalFontSize = 12.sp
-    val mediumFontSize = 14.sp
-    val lineHeight = 20.sp
     val smallSpacing = 2.dp
     val mediumSpacing = 4.dp
     val maxWidth = 210.dp
@@ -199,6 +242,22 @@ fun OtherTextMessagePreview(){
 fun MyTextMessagePreview(){
     HohoiTheme {
         Message(myTextMessageDummyData)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyReplyForTextMessagePreview(){
+    HohoiTheme {
+        Message(myReplyForTextMessageDummyData)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OtherReplyForTextMessagePreview(){
+    HohoiTheme {
+        Message(otherReplyForTextMessageDummyData)
     }
 }
 
