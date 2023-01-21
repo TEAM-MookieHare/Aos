dd95a637b5851bd6a71e2b93855d3d9a74887275
hare <khm0651@gmail.com>
2023-01-21 17:35:31 +0900

    refactor: refactoring Message Data Class

--- feature/chat/src/main/java/com/mookiehare/hohoi/feature/chat/model/Message.kt
+++ feature/chat/src/main/java/com/mookiehare/hohoi/feature/chat/model/Message.kt
@@ -1,32 +1,41 @@
 package com.mookiehare.hohoi.feature.chat.model
 
-sealed class Message()
-
-sealed class FromMy(): Message()
-
-sealed class FromOther(
-    open val nickname: String,
-    open val profileUrl: String?,
-): Message()
-
-data class MyText(
-    val id: String,
-    val content: String,
-    val isDelete: Boolean,
-    val isReport: Boolean,
-    val sendTime: String,
-): FromMy(
+sealed class Message(
+    open val id: String,
+    open val sendTime: String,
+    open val from: From
 )
 
-data class OtherText(
-    val id: String,
-    override val nickname: String,
-    override val profileUrl: String?,
+sealed class From{
+    object My : From()
+
+    data class Other(
+        val nickname: String,
+        val profileUrl: String,
+    ): From()
+}
+
+data class Text(
+    override val id: String,
+    override val sendTime: String,
+    override val from: From,
     val content: String,
-    val isDelete: Boolean,
-    val isReport: Boolean,
-    val sendTime: String,
-): FromOther(
-    nickname = nickname,
-    profileUrl = profileUrl
+): Message(
+    id = id,
+    sendTime = sendTime,
+    from = from
+)
+
+data class ReplyText(
+    override val id: String,
+    override val sendTime: String,
+    override val from: From,
+    val replyId: String,
+    val replyNickname: String,
+    val replyContent: String,
+    val content: String,
+): Message(
+    id = id,
+    sendTime = sendTime,
+    from = from
 )
