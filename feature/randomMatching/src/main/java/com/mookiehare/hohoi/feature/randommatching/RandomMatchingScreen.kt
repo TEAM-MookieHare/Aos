5aef68c22a5deebb7670c985e351aa0c10fbd8f3
HyunhwaShin <hhwa39@gmail.com>
2023-01-24 16:26:35 +0900

    feat: add chip background

--- feature/randomMatching/src/main/java/com/mookiehare/hohoi/feature/randommatching/RandomMatchingScreen.kt
+++ feature/randomMatching/src/main/java/com/mookiehare/hohoi/feature/randommatching/RandomMatchingScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -48,7 +49,9 @@ internal fun RandomMatchingScreen(
 fun RandomMatchingChips(
     elements : List<RandomChip>
 ){
-    Surface {
+    Surface(
+        color = Color.Transparent
+    ) {
         LazyRow(
             modifier = Modifier.padding(start = 10.dp, end = 10.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
